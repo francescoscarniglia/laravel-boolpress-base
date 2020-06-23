328840c3e10db36a09795b32bcd2d004dca6369e
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-24 00:19:10 +0200

    Table seeder

--- boolpress/database/seeds/ReviewTableSeeder.php
+++ boolpress/database/seeds/ReviewTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Review;
 
 class ReviewTableSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class ReviewTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $products = Product::all();
+
+        // total reviews
+        $total_reviews = 20;
+        if ($i = 0; i < $total_reviews; i++) {
+            $newReview = new Review();
+            $newReview ->product_id = $products->random()->id;
+            $newReview->title = $faker->text(10);
+
+        }
     }
 }
